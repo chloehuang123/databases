4cd2c22879e683bfbc0337f0201004fbc465aebc
chloe <312864100@qq.com>
2022-03-17 23:23:18 -0400

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -4,11 +4,25 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  text_message VARCHAR(200) NOT NULL,
+  room_id INT NOT NULL,
+  user_id INT NOT NULL
 );
 
 /* Create other tables and define schemas for them here! */
 
+CREATE TABLE rooms (
+  /* Describe your table here.*/
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+room VARCHAR(200) NOT NULL
+);
 
+CREATE TABLE users (
+  /* Describe your table here.*/
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+user VARCHAR(200) NOT NULL
+);
 
 
 /*  Execute this file from the command line by typing:
